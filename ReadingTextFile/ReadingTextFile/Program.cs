ab7b9e5d050b0fe315f6419d897d9181bdca6dec
Gandholi Saiarunkumar <46054848+gandholi-kumar@users.noreply.github.com>
2019-01-09 09:43:21 +0530

    my first upload
    sample files were updated

---
+++ ReadingTextFile/ReadingTextFile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingTextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\hp\Desktop\space.txt";
+
+            using (var reader = new StreamReader(filePath))
+            {
+                int spaceCount = 0;
+                string line;
+                while(( line = reader.ReadLine()) != null)
+                {
+                    if (line.Contains(" "))
+                    {
+                        for (int i = 0; i < line.Length-1; i++)
+                        {
+                            if(line[i]==' ')
+                            {
+                                spaceCount+=1;
+                            }
+                        }
+                    }            
+                }
+                Console.WriteLine(spaceCount);
+            }
+        }
+    }
+}
